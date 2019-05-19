77210586fc74e5f298c9a425d0e0da8496e4688d
Marty Mathis <Marty.Mathis@assurant.com>
2019-05-19 09:21:01 -0400

    Completed Lamar unit tests

---
+++ tests/PipelineFramework.Lamar.Tests/PipelineRegistryTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Lamar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PipelineFramework.Abstractions;
+using PipelineFramework.Lamar.Tests.Infrastructure;
+using PipelineFramework.Tests.SharedInfrastructure;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PipelineFramework.Lamar.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class PipelineCompositionRootTests
+    {
+        private readonly IContainer _container;
+
+        public PipelineCompositionRootTests()
+        {
+            _container = new Container(new TestPipelineRegistry());
+        }
+
+        [TestMethod]
+        public void PipelineCompositionRoot_GetPipeline()
+        {
+            var result = _container.GetInstance<IPipeline<TestPayload>>();
+
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void PipelineCompositionRoot_GetAsyncPipeline()
+        {
+            var result = _container.GetInstance<IAsyncPipeline<TestPayload>>();
+
+            result.Should().NotBeNull();
+        }
+    }
+}
